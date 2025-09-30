e80596e6b93f1bf4b0f318fd98f6a069248b8857
s.wojcik <sebastianwojcik113@gmail.com>
2025-09-30 23:05:05 +0200

    main.py: - added argument parsing to pass the test script filename and DUT adb serial number as arguments when running the test execution

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import socket
 import subprocess
@@ -14,15 +15,22 @@ if __name__ == '__main__':
     DEVICE_PORT = 5557  # port on DUT
     EXPECTED_OWNER = "admin=com.example.testrunner/.MyDeviceAdminReceiver"
 
+    #Parse arguments (test script, adb serial?)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--script", "-s", required=True, type=str)
+    parser.add_argument("--adb_serial", "-a", required=True, type=str)
+    args = parser.parse_args()
+    script = args.script
+    dut_serial = args.adb_serial
+
     # Check port forwarding to communicate with DUT
-    do_handler = DeviceOwnerHandler(host_port=HOST_PORT, device_port=DEVICE_PORT, expected_owner=EXPECTED_OWNER, adb_serial=ADB_SERIAL)
+    do_handler = DeviceOwnerHandler(host_port=HOST_PORT, device_port=DEVICE_PORT, expected_owner=EXPECTED_OWNER, adb_serial=dut_serial)
     do_handler.forward_port(HOST_PORT,DEVICE_PORT)
     # Check if DO is already set
     do_handler.check_device_owner(EXPECTED_OWNER)
 
     # Start main Test Manager work
-
     testManager = TestManager()
-    commands = testManager.load_commands_from_file('test_scenario.json')
+    commands = testManager.load_commands_from_file(script)
     if commands != None:
         testManager.run_test_sequence(commands)
